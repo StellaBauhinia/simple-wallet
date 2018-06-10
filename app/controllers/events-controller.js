cfddd939734097757673a55d57ac78afa83bf174
Stella <stellar@inschains-mbp.zte.com.cn>
2018-06-10 10:02:42 +0800

    add network_id adapter for etherscan prefix

--- app/controllers/events-controller.js
+++ app/controllers/events-controller.js
@@ -19,37 +19,45 @@ function EventsController($scope) {
       eventsToWatch.watch(function(error, result) {
         if (!error) {
           var eventType=result.event;
-          result.url="https://etherscan.io/tx/"+result.transactionHash;
-          switch(eventType){
-            case "Deposit":
-              result.args.value=web3.fromWei(result.args.value.toNumber(), "ether");
-              vm.depositEvents.push(result);
-              break;
-            case "Execution":
-              result.args.value=web3.fromWei(result.args.value, "ether");
-              vm.etherEvents.push(result);
-              break;
-            case "Confirmation":
-              vm.pendingEvents.push(result);
-              break;
-            case "Revocation":
-              vm.pendingEvents.push(result);
-              break;
-            case "OwnerAddition":
-              vm.adminEvents.push(result);
-              break;
-            case "OwnerRemoval":
-              vm.adminEvents.push(result);
-              break;
-            case "RequirementChange":
-              vm.requireEvents.push(result);
-              break;
-            default:
-              vm.otherEvents.push(result);
-          }
+          SimpleWallet.detectNetwork().then(function(network_id,err){
+            if (network_id=="4")
+              prefix = "https://rinkeby.etherscan.io/tx/"
+            else if (network_id=="1")
+              prefix = "https://etherscan.io/tx/"
 
-          //console.log(vm.events);
-          $scope.$apply();
+            result.url=prefix+result.transactionHash;
+            switch(eventType){
+              case "Deposit":
+                result.args.value=web3.fromWei(result.args.value.toNumber(), "ether");
+                vm.depositEvents.push(result);
+                break;
+              case "Execution":
+                result.args.value=web3.fromWei(result.args.value, "ether");
+                vm.etherEvents.push(result);
+                break;
+              case "Confirmation":
+                vm.pendingEvents.push(result);
+                break;
+              case "Revocation":
+                vm.pendingEvents.push(result);
+                break;
+              case "OwnerAddition":
+                vm.adminEvents.push(result);
+                break;
+              case "OwnerRemoval":
+                vm.adminEvents.push(result);
+                break;
+              case "RequirementChange":
+                vm.requireEvents.push(result);
+                break;
+              default:
+                vm.otherEvents.push(result);
+            }
+
+            //console.log(vm.events);
+            $scope.$apply();
+          });
+          
         }else{
           console.log(error);
         }
